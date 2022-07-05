abac76dd7c16a862683b7d1a33d246f17cba2e9c
AGBAGHER JAPHET <ternguy1995@gmail.com>
2022-07-05 07:54:22 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *i _abs -> return abosolute value of numbers.
+ * @n: parameters.
+ * Return: The absolute value of the number.
+ */
+int _abs(int)
+{
+	if (n < 0)
+	n = -n
+	return (n);
+}
