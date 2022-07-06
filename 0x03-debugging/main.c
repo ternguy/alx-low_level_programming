2ba70be19f7f9f094de96cac14430f0a45ac1ef4
AGBAGHER JAPHET <ternguy1995@gmail.com>
2022-07-06 04:15:49 -0700

    main - prints if a number is negative, zero and negative

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -1,26 +1,30 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
+/**
+ * test_positive_or_negative -> test positive or negative,
+ *  when given a case of zero
+ */
 void positive_or_negative(int i)
 {
 	 int n;
 
 	 srand(time(0));
 
-	 n = i;
-	 if (n > 0)
-	 {
-		 printf("%d is positive\n", n);
-	 }
-	 else if (n == 0)
-	 {
-		 printf("%d is zero\n", n);
-	 }
-	 else
-	 {
-		 printf("%d is negative\n", n);
-	 }
-	 return (0);
+	n = i;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
 }
 /**
  * main - debuggging example
