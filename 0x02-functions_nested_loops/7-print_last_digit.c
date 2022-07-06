ab3b743b5db101e4b70c4cbd7c543eb62dfcaa63
AGBAGHER JAPHET <ternguy1995@gmail.com>
2022-07-06 01:29:58 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,12 +6,11 @@
  */
 int print_last_digit(int n)
 {
-	int last_digit = n % 10;
+	int x;
 
-	if (last_digit < 0)
-	{
-		last_digit *= -1;
-		_putchar(last_digit + '0');
-	}
-	return (last_digit);
+	if (n < 0)
+		n = -n;
+	x = n % 10;
+	_putchar(x + '0');
+	return (x);
 }
