94ff182bf2e48722eeb6c70bc2193e6016eaf9ba
AGBAGHER JAPHET <ternguy1995@gmail.com>
2022-07-07 00:38:46 -0700

    done with debugging task 0.

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * positive_or_negative - check the code
+ * @i: print variables
+ * return: always 0
+ */
+
+void positive_or_negative(int i)
+{
+	 int n;
+
+	 srand(time(0));
+	 n = i;
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
+
+/**
+ * main - debugging example
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+
+}
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
+#include <stdio.h>
 
 void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
 #endif /* MAIN_H */
