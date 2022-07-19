c338218382e4fcf4a75093f90d824da682dae0a8
AGBAGHER JAPHET <ternguy1995@gmail.com>
2022-07-19 03:31:06 -0700

    done

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _print_rev_recursion - prints a string in reverse
  * @s: pointer block of memory to fill
@@ -8,9 +7,8 @@
 
 void _print_rev_recursion(char *s)
 {
-if (*s)
+if (*s) /*If statement*/
 {
-_print_rev_recursion(s + 1);
-_putchar(*s);
-}
+_print_rev_recursion(s + 1); /*add s*/
+_putchar(*s); /*print s*/
 }
