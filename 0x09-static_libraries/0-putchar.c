56158294769145e8ba9abf0af880ecb403ae31cc
AGBAGHER JAPHET <ternguy1995@gmail.com>
2022-07-22 07:07:14 -0700

    done

--- 0x09-static_libraries/0-putchar.c
+++ 0x09-static_libraries/0-putchar.c
@@ -1,37 +1,19 @@
 #include "main.h"
 
 /**
-
  * main - print  the file _putchar
-
  * Return: aways (success)
-
  */
-
 int main(void)
-
 {
-
 	_putchar('_');
-
 	_putchar('p');
-
 	_putchar('u');
-
 	_putchar('t');
-
 	_putchar('c');
-
 	_putchar('h');
-
 	_putchar('a');
-
 	_putchar('r');
 
-	_putchar('\n');
-
-
-
 	return (0);
-
 }
