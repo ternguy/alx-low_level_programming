0cd92ca1fff903fd11002601ab8859e4c05100d0
AGBAGHER JAPHET <ternguy1995@gmail.com>
2022-07-25 04:00:57 -0700

    done with mollac task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,68 +1,27 @@
 #include "main.h"
-/**
- *  _strlen - count array
- *  @s: array of elements
- *  Return: 1
- */
-
-int _strlen(char *s)
-{
-unsigned int i;
-
-i = 0;
-while (s[i] != '\0') /*count character of string*/
-{
-i++;
-}
-
-return (i);
-}
 
 /**
- *  _strcpy - copy arrays
- *  @src: array of elements
- *  @dest: dest array
- *  Return: dest
+ * _strdup -> string duplicator function
+ * @str: string to be duplicated
+ * Return: a string pointer
  */
-
-char *_strcpy(char *dest, char *src)
-{
-int i = 0;
-
-while (src[i] != '\0')
-{
-dest[i] = src[i];
-i++;
-{
-dest[i] = '\0';
-return (dest);
-}
-
-/**
- *  _strdup - array for prints a string
- *  @str: array of elements
- *  Return: pointer
- */
-
 char *_strdup(char *str)
 {
-char *dst;
-unsigned int size;
+	int i = 1, j = 0;
+	char *s;
 
-if (str == 0)
-{
-return (NULL);
+	if (str == NULL)
+		return (NULL);
+	while (str[i])
+		i++;
+	s = (char *) malloc(i * sizeof(char) + 1);
+	if (s == NULL)
+		return (NULL);
+	while (j < i)
+	{
+		s[j] = str[j];
+		j++;
+	}
+	s[j] = '\0';
+	return (s);
 }
-
-size = _strlen(str) + 1;
-
-dst = (char *) malloc(size *sizeof(char));
-
-if (dst == 0)
-{
-return (NULL);
-}
-_strcpy(dst, str);
-return (dst);
-}
-
