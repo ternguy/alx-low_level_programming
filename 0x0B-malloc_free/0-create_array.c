f3820521d406c69fa78055b292f169aeed8f8eaf
AGBAGHER JAPHET <ternguy1995@gmail.com>
2022-07-25 01:41:34 -0700

    doe with mollacs free task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * create_array - array for prints a string
- * @size: number elements array
- * @c: char
- * Return: pointer
+ *  create_array - array for prints a string
+ *  @size: number elements array
+ *  @c: char
+ *  Return: pointer
  */
 
 char *create_array(unsigned int size, char c)
