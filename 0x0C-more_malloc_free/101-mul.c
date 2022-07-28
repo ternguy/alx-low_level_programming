ef5263a0772f0412d8e65d46edf04718b22aa00f
AGBAGHER JAPHET <ternguy1995@gmail.com>
2022-07-28 07:06:53 -0700

    done

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -62,7 +62,7 @@ return (dest);
 * check_for_digits - checks the arguments to ensure they are digits
 * @av: pointer to arguments
 * Return: 0 if digits, 1 if not
- */
+*/
 int check_for_digits(char **av)
 {
 int i, j;
@@ -99,24 +99,23 @@ str[i] = '\0';
  *  @argv: argument vector
  *  Return: zero, or exit status of 98 if failure
  */
-
 int main(int argc, char *argv[])
 {
 int l1, l2, ln, ti, i;
 char *a;
 char *t;
 char e[] = "Error\n";
-
 if (argc != 3 || check_for_digits(argv))
 {
 for (ti = 0; e[ti]; ti++)
 _putchar(e[ti]);
 exit(98);
 }
-for (l1 = 0; argv[1][l1]; l1++);
-for (l2 = 0; argv[2][l2]; l2++);
+for (l1 = 0; argv[1][l1]; l1++)
+;
+for (l2 = 0; argv[2][l2]; l2++)
+;
 ln = l1 + l2 + 1;
-
 a = malloc(ln * sizeof(char));
 if (a == NULL)
 {
