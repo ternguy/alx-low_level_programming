022680ba2bbc6db96153985903ba9c713e902349
AGBAGHER JAPHET <ternguy1995@gmail.com>
2022-07-28 06:26:52 -0700

    done with the advanced

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,5 @@
 #include "main.h"
-#include <stdlib.h>
-
+#include <stdio.h>
 /**
  *  _print - moves a string one place to the left and prints the string
  *  @str: string to move
@@ -15,6 +14,7 @@ i = j = 0;
 while (i < l)
 {
 if (str[i] != '0')
+
 j = 1;
 if (j || i == l - 1)
 _putchar(str[i]);
@@ -113,11 +113,10 @@ for (ti = 0; e[ti]; ti++)
 _putchar(e[ti]);
 exit(98);
 }
-for (l1 = 0; argv[1][l1]; l1++)
-;
-for (l2 = 0; argv[2][l2]; l2++)
-;
+for (l1 = 0; argv[1][l1]; l1++);
+for (l2 = 0; argv[2][l2]; l2++);
 ln = l1 + l2 + 1;
+
 a = malloc(ln * sizeof(char));
 if (a == NULL)
 {
@@ -126,10 +125,10 @@ _putchar(e[ti]);
 exit(98);
 }
 init(a, ln - 1);
-for (ti = l2 - 1, i = 0; ti >= 0; ti--, i++);
+for (ti = l2 - 1, i = 0; ti >= 0; ti--, i++)
 {
 t = mul(argv[2][ti], argv[1], l1 - 1, a, (ln - 2) - i);
-if (t == NULL);
+if (t == NULL)
 {
 for (ti = 0; e[ti]; ti++)
 _putchar(e[ti]);
