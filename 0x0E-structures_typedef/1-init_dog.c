69169afbe9795a6ff084f2a79d65beeb2571c0f6
AGBAGHER JAPHET <ternguy1995@gmail.com>
2022-08-01 04:28:09 -0700

    struct typedef

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <sdtlib.h>
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog *d
+	{
+		char *name;
+		float age;
+		char *owner;
+	}
+}
+
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+int main(void)
+{
+	struct dog dogs;
+	return(0);
+}
