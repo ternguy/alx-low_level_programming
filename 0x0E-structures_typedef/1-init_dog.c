d4e43390713cc82551ec5dd7429aa28ff77dbc42
AGBAGHER JAPHET <ternguy1995@gmail.com>
2022-08-01 10:49:54 -0700

    done

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -20,5 +20,9 @@ struct dog
 /**
  * dog_t - typedef for struct dog
  */
-typedef struct dog dog_t($dog_t,"poppy", "Pardic", 4);
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t = new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
