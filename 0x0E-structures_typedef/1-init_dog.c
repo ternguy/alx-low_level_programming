eed61816e3a2d4619fb82f0c5c0362563afb54ca
AGBAGHER JAPHET <ternguy1995@gmail.com>
2022-08-01 13:48:50 -0700

    done

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,21 +1,20 @@
-#include <stdio.h>                                                                                                                    
-#include <stdlib.h>                                                                                                                   
-#include "dog.h"                                                                                                                      
-                                                                                                                                      
-/**                                                                                                                                   
- *init_dog - initialize the dog struct                                                                                                
- * struct dog - a new type of data                                                                                                    
- * @b: the dog struct                                                                                                                 
- * @name: name of doggg                                                                                                               
- * @age: age of dog                                                                                                                   
- * @owner: owner of dog                                                                                                               
- */                                                                                                                                   
-                                                                                                                                      
-void init_dog(struct dog *d, char *name, float age, char *owner)                                                                      
-{                                                                                                                                     
-if (d == NULL)                                                                                                                        
-        return (NULL);                                                                                                                
-        d->name = name;                                                                                                               
-        d->age = age;                                                                                                                 
-        d->owner =owner;                                                                                                              
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - initialize the dog struct
+ * struct dog - a new type of data
+ * @d: the dog struct
+ * @name: name of doggg
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
