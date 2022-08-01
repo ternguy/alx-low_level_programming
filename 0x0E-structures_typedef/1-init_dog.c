0a9c4644909f06624c7d194da9c1f6492db18e4a
AGBAGHER JAPHET <ternguy1995@gmail.com>
2022-08-01 09:16:50 -0700

    done

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,19 +1,29 @@
+#ifndef DOG_H
+#define DOG_H
+
 #include <stdio.h>
-#include <sdtlib.h>
+#include <stdlib.h>
+/**
+ * struct dog - a new type of data
+ * representating a dog
+ * @name: name of doggg
+ * @age: age of dog
+ * @owner: owner of dog
+ */
 
-
-void init_dog(struct dog *d, char *name, float age, char *owner)
+struct dog
 {
-	struct dog *d
-	{
-		char *name;
-		float age;
-		char *owner;
-	};
-	struct dog;
-	void main()
-	{
-		
-	}
-}
-
+	char *name;
+	char *owner;
+	float age;
+};
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+void int_dog(struct dog *d, char *name, floate age, char *owner);
+void print_dog(struct dog *d);
+dog_t = *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+int_strlen(char *strn);
+char *_strcopy(char *dest, char *str);
