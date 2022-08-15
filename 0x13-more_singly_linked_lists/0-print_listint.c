a8c78f85aac88ca8185739250b2cf03255637b6d
AGBAGHER JAPHET <ternguy1995@gmail.com>
2022-08-15 13:43:38 -0700

    done

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,11 +1,8 @@
 #include "lists.h"
 
-
 /**
  * print_listint - prits all the elements of a linked list
-
  * @h: head of the list
- *
  * Return: the number of nodes
  */
 size_t print_listint(const listint_t *h)
--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -14,7 +14,7 @@ listint_t *node1, *node2;
 if (head == NULL || head->next == NULL)
 return (NULL);
 
-node1 = head->next;
+node1 = head->next
 node2 = (head->next)->next;
 
 while (node2)
@@ -25,4 +25,16 @@ node1 = head;
 
 while (node1 != node2)
 {
+node1 = node1->next;
+node2 = node2->next;
+}
 
+return (node1);
+}
+
+node1 = node1->next;
+node2 = (node2->next)->next;
+}
+
+return (NULL);
+}
