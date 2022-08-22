e4a16340fdc00364d670978892349c17c56b68c7
AGBAGHER JAPHET <ternguy1995@gmail.com>
2022-08-22 06:44:22 -0700

    error fixed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,14 +4,13 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-
 /**
  * _strlen - finds the length of a string
  * @str: pointer to the string
  *
  * Return: length of the string
  */
-size_t _strlen(char *str)
+int _strlen(char *str)
 {
 	size_t i;
 
@@ -21,25 +20,25 @@ size_t _strlen(char *str)
 }
 
 /**
- * create_file - creates a file.
- * @filename: name of the file to create
- * @text_content: NULL terminated string to write to the file
+ * append_text_to_file - appends a text at the end of a file.
+ * @filename: name of the file
+ * @text_content: NULL terminated string to add at the end of the file
  *
- * Return: 1 on success, -1 on failure
+ * Return: 1 on success and -1 on failure
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t len = 0;
+	ssize_t len;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)
 		len = write(fd, text_content, _strlen(text_content));
-	close(fd);
+ 	close(fd);
 	if (len == -1)
 		return (-1);
 	return (1);
