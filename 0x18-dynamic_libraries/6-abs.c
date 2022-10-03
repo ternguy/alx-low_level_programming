ad7f20823df5fd575dd1c24eea42938c830a101c
AGBAGHER JAPHET <ternguy1995@gmail.com>
2022-10-03 04:05:54 -0700

    done

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs -> return abosolute value of numbers.
+ * @n: parameters.
+ * Return: absolute value of the number.
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	return (n);
+	else
+	return (-n);
+}
